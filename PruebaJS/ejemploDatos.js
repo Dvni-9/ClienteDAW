5e61ea628b5ca69619ff87cdaf8536b1b702e0f1
Daniel <danielsanmiguellopez@gmail.com>
2026-09-17 08:35:56 +0200

    Subo lo que ya tenia hecho, ya que no me deja subirlo por partes.

---
+++ PruebaJS/ejemploDatos.js
@@ -0,0 +1,18 @@
+let numero1 = 10;               
+console.log(typeof numero1);
+let numero2 = 1.03;             
+console.log(typeof numero2);
+let booleano = true;            
+console.log(typeof booleano);
+let cadena="Hola mundo";        
+console.log(typeof cadena);
+let nulo = null;                
+console.log(typeof nulo);
+                                
+console.log(nulo);
+let indefinido;                 
+console.log(typeof indefinido);
+                                
+console.log(indefinido);
+let obejeto={a:1};              
+console.log(typeof obejeto);
---
+++ PruebaJS/main.js
@@ -0,0 +1,9 @@
+console.log("Hola mundo");
+
+let x= 5;
+let y = 2;
+let z = x + y;
+console.log(z);
+
+let w = x/0;
+console.log(w);
