ec4315873d23295da03e371d40442ece07a8dc0a
Daniel <danielsanmiguellopez@gmail.com>
2026-09-18 08:49:17 +0200

    Commit

--- PruebaJS/holaMundo.js
+++
@@ -1,9 +0,0 @@
-console.log("Hola Mundo!");
-var x=5;
-var y=10;
-var z=x+y;
-console.log(z);
-
-var w =0;
-console.log(x/w);
-
--- PruebaJS/main.js
+++ PruebaJS/main.js
@@ -1,9 +1,1 @@
 console.log("Hola mundo");
-
-let x= 5;
-let y = 2;
-let z = x + y;
-console.log(z);
-
-let w = x/0;
-console.log(w);
